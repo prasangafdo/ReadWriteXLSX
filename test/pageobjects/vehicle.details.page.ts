a70ac3d8663e38c91d6e2aa10527d307cadde601
Prasanga Fernando <prasangafdz@outlook.com>
2022-07-11 09:57:30 +0530

    Update vehicle.details.page.ts

---
+++ test/pageobjects/vehicle.details.page.ts
@@ -0,0 +1,13 @@
+import Page from './page';
+
+class VehicleDetailsPage extends Page {
+
+
+//h1[@class='title--3s1R8']
+//a[@data-testid='subtitle-sublocation-link']/span
+//div[@class='amount--3NTpl']
+//div[text()='Year of Manufacture: ']//following-sibling::div//span
+
+}
+
+export default new VehicleDetailsPage();
