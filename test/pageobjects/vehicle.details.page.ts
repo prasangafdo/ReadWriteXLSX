b98779fc5412b53a38b90e7ea59f7bf58eb4a7be
Prasanga Fernando <prasangafdz@outlook.com>
2022-07-11 10:06:40 +0530

    Added element locators

--- test/pageobjects/vehicle.details.page.ts
+++ test/pageobjects/vehicle.details.page.ts
@@ -2,11 +2,28 @@ import Page from './page';
 
 class VehicleDetailsPage extends Page {
 
+    get lblAdTitle(){
+        return $("//h1[@class='title--3s1R8']")
+    }
+    get lblLocation(){
+        return $("//a[@data-testid='subtitle-sublocation-link']/span")
+    }
+    get lblPrice(){
+        return $("//div[@class='amount--3NTpl']")
+    }
+    get lblYear(){
+        return $("//div[text()='Year of Manufacture: ']//following-sibling::div//span")
+    }
+    get btnShowMore(){
+        return $("//button[normalize-space()='Show more']")
+    }
+    get btnShowPhoneNumber(){
+        return $("//div[@class='display--s3dc8 card--_2NNk']//button[contains(@class,'show-number')]")
+    }
 
-//h1[@class='title--3s1R8']
-//a[@data-testid='subtitle-sublocation-link']/span
-//div[@class='amount--3NTpl']
-//div[text()='Year of Manufacture: ']//following-sibling::div//span
+
+
+
 
 }
 
