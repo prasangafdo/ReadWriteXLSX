2d74d5c71ec77be48b6e2e468f732df640c5add3
Prasanga Fernando <prasangafdz@outlook.com>
2022-07-16 19:24:20 +0530

    Update DDTManager.ts
    Resolved promise returned by 'getAllAds' function

--- test/util/DDTManager.ts
+++ test/util/DDTManager.ts
@@ -1,5 +1,6 @@
 import ExcelReader from '../util/ExcelReader';
 import Vehicles from '../pageobjects/vehicles.page';
+import VehicleDetails from '../pageobjects/vehicle.details.page';
 const testDataFile = './test/testdata/TestData.xlsx'
 
 class DDTManager{
@@ -29,6 +30,22 @@ class DDTManager{
             await Vehicles.txtMinPrice.setValue(data.min_price)//Add a clear value
             await Vehicles.txtMaxPrice.setValue(data.max_price)
             await Vehicles.btnApplyPrice.click()
+            let elementsArray = []
+            await Vehicles.getAllAds().then(function (ttt) {
+                console.log('-----<', ttt)
+                for (let element of ttt){
+                    elementsArray.push(element)
+                }
+            })
+            console.log('+++++++++<',elementsArray)
+            console.log('+++++++++<',await elementsArray[0].getText())
+
+            // for (let vehicle of aaa){
+            //     console.log('}}}}}}}}}}}}}}}}',await vehicle.getText())
+            //     await vehicle.click()
+            //     console.log('--->',await VehicleDetails.lblAdTitle.getText())
+            //     await browser.back()
+            // }
             await browser.pause(5000)
 
             // console.log('---------District', await Vehicles.getLnkDistrict(data.District).click())
